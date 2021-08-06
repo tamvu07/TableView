7ad3b259d48328c1377717b6d18f13f1f3df7798
Tam Vu <tam.vm@aris-vn.com>
2021-08-06 16:29:52 +0700

    +Create project

---
+++ TableView/TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Vu Minh Tam on 8/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
