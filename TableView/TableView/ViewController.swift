3db4f1878211f2e7aaf57aa7cc997fb44ce98119
Tam Vu <tam.vm@aris-vn.com>
2021-08-09 09:11:45 +0700

    +Update code with table can remove and change color (https://www.youtube.com/watch?v=kBGamF_rL1Q&list=PL5PR3UyfTWvcF_lDzMbDVbm-iwXHJeEvn&index=20)

--- TableView/TableView/ViewController.swift
+++ TableView/TableView/ViewController.swift
@@ -7,13 +7,113 @@
 
 import UIKit
 
+class User {
+    let name: String
+    var isFavorite: Bool
+    var isMuted: Bool
+    
+    init(name: String, isFavorite: Bool, isMuted: Bool) {
+        self.name = name
+        self.isFavorite = isFavorite
+        self.isMuted = isMuted
+    }
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+ 
+    var users: [User] = [
+        "John Smith",
+        "Dan Smith",
+        "Ben Smith",
+        "Rob Smith",
+        "Amy Smith",
+        "Bill Smith",
+    ].compactMap({
+        User(name: $0,
+             isFavorite: false,
+             isMuted: false)
+    })
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
 }
 
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("abc")
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let deleteAction = UITableViewRowAction(
+            style: .destructive,
+            title: "Delete")
+            { _, indexPath in
+                self.users.remove(at: indexPath.row)
+                self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            }
+        
+        let user = users[indexPath.row]
+        let favoriteActionTitle = user.isFavorite ? "Unfavorite" : "Favorite"
+        let muteActionTitle = user.isMuted ? "Unmute" : "Mute"
+        
+        let favoriteAction = UITableViewRowAction(
+            style: .normal,
+            title: favoriteActionTitle)
+            { _, indexPath in
+            self.users[indexPath.row].isFavorite.toggle()
+            self.tableView.reloadRows(at: [indexPath], with: .automatic)
+            }
+        
+        let muteAction = UITableViewRowAction(
+            style: .normal,
+            title: muteActionTitle)
+            { _, indexPath in
+            self.users[indexPath.row].isMuted.toggle()
+            self.tableView.reloadRows(at: [indexPath], with: .automatic)
+            }
+        
+        favoriteAction.backgroundColor = .systemBlue
+        muteAction.backgroundColor = .systemOrange
+        
+        return [deleteAction, favoriteAction, muteAction]
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let user = users[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = users[indexPath.row].name
+        if user.isFavorite {
+            cell.backgroundColor = .systemBlue
+        }
+        else if user.isMuted {
+            cell.backgroundColor = .systemOrange
+        }
+        else {
+            cell.backgroundColor = nil
+        }
+        return cell
+    }
+}
